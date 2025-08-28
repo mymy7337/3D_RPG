cc425bbd0dc77b01f2dd551a55d533bbea0e24a6
mymy7337 <minyeong7337@gmail.com>
2025-08-28 12:58:39 +0900

    [Feat] 장비 데이터 추가

---
+++ Assets/04. ScriptableObjects/Item/ItemDataSO.cs
@@ -0,0 +1,23 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public enum ItemType
+{
+    Weapon,
+    Armor,
+    Expend
+}
+
+[CreateAssetMenu(fileName ="Item", menuName = "New Item")]
+public class ItemDataSO : ScriptableObject
+{
+    [Header("Info")]
+    public string itemName;
+    public string description;
+    public float value;
+    public float price;
+    public Sprite icon;
+
+    [Header("Type")]
+    public ItemType type;
+}
